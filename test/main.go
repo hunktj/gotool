80ec739623762b0b172c1b3f4966adfb73e7545b
tianjun <tianjun@kuaibang360.com>
2022-10-17 09:56:54 +0800

    one

--- test/main.go
+++ test/main.go
@@ -1,17 +1,22 @@
 package main
 
-func main() {
-	// //ils := gotools.ToString(23)
-	// num := int64(34)
-	// sdd := gotool.ToInt(num)
-	// //r := gotools.Rounding(99449.543556, 3)
-	// tmp := "2006-01-02 15:04:05"
-	// //tm := gotools.UnixTimeToStr(1661843160, tmp)
-	// stm, _ := gotool.StrToUnix("2022-08-30 15:09:16", tmp)
-	// nowtime := gotool.GetNowTime()
-	// smp, emp := gotool.DayStimeAndEtime(nowtime)
+import (
+	"fmt"
+	"github.com/hunktj/gotool"
+)
 
-	// nowunixtime := gotool.GetNowTimeUinx()
-	// //startTime, endTime := gotools.GetTime("2022-06")
-	// fmt.Println(stm, sdd, smp, emp, nowtime, nowunixtime)
+func main() {
+	//ils := gotools.ToString(23)
+	num := int64(34)
+	sdd := gotool.ToInt(num)
+	//r := gotools.Rounding(99449.543556, 3)
+	tmp := "2006-01-02 15:04:05"
+	//tm := gotools.UnixTimeToStr(1661843160, tmp)
+	stm, _ := gotool.StrToUnix("2022-08-30 15:09:16", tmp)
+	nowtime := gotool.GetNowTime()
+	smp, emp := gotool.DayStimeAndEtime(nowtime)
+
+	nowunixtime := gotool.GetNowTimeUinx()
+	//startTime, endTime := gotools.GetTime("2022-06")
+	fmt.Println(stm, sdd, smp, emp, nowtime, nowunixtime)
 }
